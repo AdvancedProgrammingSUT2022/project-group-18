37c906a32f7a3cf7356c30cc844cb3697d3fc0fd
fatemeh <me.mousavi1382@gmail.com>
2022-04-21 18:51:41 +0430

    last

--- civilization/src/main/java/controller/MainMenuController.java
+++ civilization/src/main/java/controller/MainMenuController.java
@@ -5,5 +5,9 @@ import com.google.gson.internal.bind.util.ISO8601Utils;
 public class MainMenuController {
     int the;
     int x;
+    int sl;
+    public void newOne() {
+        System.out.println("every thing is ok!");
+    }
 
 }
