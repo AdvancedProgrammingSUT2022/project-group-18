cdef89a43ef92a2786c54737b05bdc8e419c8587
fatemeh <me.mousavi1382@gmail.com>
2022-05-09 08:00:57 +0430

    a unit for improvements

--- civilization/src/main/java/enums/ImprovementsEnum.java
+++ civilization/src/main/java/enums/ImprovementsEnum.java
@@ -1,2 +1,53 @@
-package enums;public enum ImprovementsEnum {
+package enums;
+
+import model.Improvement;
+
+public enum ImprovementsEnum {
+    CAMP("Camp"),
+    FARM("Farm"),
+    LUMBER_MILL("LumberMill"),
+    MINE("Mine"),
+    PASTURE("Pasture"),
+    PLANTATION("Plantation"),//kesht va kar
+    QUARRY("Quarry"),//madan sang
+    TRADING_POST("TradingPost"),
+    FACTORY("Factory");
+    private String regex;
+
+    ImprovementsEnum(String regex) {
+        this.regex = regex;
+    }
+
+    public static Improvement getImprovements(ImprovementsEnum improvement) {
+        switch (improvement) {
+            case CAMP:
+                if (TechsEnum.technologyCheck(TechsEnum.TRAPPING))
+                    return new Improvement("Camp",0, 0, 0);
+            case FARM:
+                if (TechsEnum.technologyCheck(TechsEnum.AGRI_CULTURE))
+                    return new Improvement("Farm",1, 0, 0);
+            case LUMBER_MILL:
+                if (TechsEnum.technologyCheck(TechsEnum.CONSTRUCTION))
+                    return new Improvement("LumberMill",0, 1, 0);
+            case MINE:
+                if (TechsEnum.technologyCheck(TechsEnum.MINING))
+                    new Improvement("Mine",0, 1, 0);
+            case PASTURE:
+                if (TechsEnum.technologyCheck(TechsEnum.ANIMAL_HUSBANDRY))
+                    new Improvement("Pasture",0, 0, 0);
+            case PLANTATION:
+                if (TechsEnum.technologyCheck(TechsEnum.CALENDAR))
+                    new Improvement("Plantation",0, 0, 0);
+            case QUARRY:
+                if (TechsEnum.technologyCheck(TechsEnum.MASONRY))
+                    new Improvement("Quarry",0, 0, 0);
+            case TRADING_POST:
+                if (TechsEnum.technologyCheck(TechsEnum.TRAPPING))
+                    new Improvement("TradingPost",0, 0, 1);
+            case FACTORY:
+                if (TechsEnum.technologyCheck(TechsEnum.ENGINEERING))
+                    new Improvement("Factory",0, 2, 0);
+        }
+        return null;
+    }
 }
