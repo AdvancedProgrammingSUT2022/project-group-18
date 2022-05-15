40626a023afe04b4cb3975ca61679d2911aec071
fatimasd003 <94687837+fatimasd003@users.noreply.github.com>
2022-05-15 14:07:29 +0430

    Add files via upload

--- civilization/src/main/java/model/unit/Worker.java
+++ civilization/src/main/java/model/unit/Worker.java
@@ -11,86 +11,105 @@ public class Worker extends  Unit{
     }
 
 
-    public static void foundRoad(int tile,hex map) {
+    public static booleanfoundRoad(int tile,hex map) {
         if((hex.randLand[tile]!=3 )&&(hex.randLand[tile]!= 5 ) &&(hex.randLand[tile]!=6)){
             //creat road
             //graphic
+            return  true;
 
         }
+        else
+            return false;
 
 
     }
-    public void foundRailway(int tileId,hex map,Tile tile) {
+    public static boolean foundRailway(int tileId,hex map,Tile tile) {
         if ((map.randLand[tileId] != 3) && (map.randLand[tileId] != 5) && (map.randLand[tileId] != 6)) {
+
             //creat railway
             //graphic
+            return  true;
         }
+        else
+            return false;
     }
-        public void foundFarm(int tileId,hex map,Tile tile){
+        public static void foundFarm(int tileId,hex map,Tile tile){
             //creat railway
             //graphic
         }
 
 
-    public void foundMine(int tileId,hex map,Tile tile) {
+    public static void foundMine(int tileId,hex map,Tile tile) {
       //creat railway
         //graphic
     }
-    public void foundTradingPost(int tileId,hex map,Tile tile) {
+    public static void foundTradingPost(int tileId,hex map,Tile tile) {
         //creat railway
         //graphic
         tile.incraerseGold(1);
 
     }
-    public void foundWoodCuting(int tileId,hex map,Tile tile) {
+    public static void foundFarm(int tileId,hex map,Tile tile) {
+
+        //creat railway
+        //graphic
+
+
+    }
+    public static void foundWoodCuting(int tileId,hex map,Tile tile) {
         //creat railway
         //graphic
     }
-    public void foundPasture(int tileId,hex map,Tile tile) {
+    public static  void foundPasture(int tileId,hex map,Tile tile) {
         //creat railway
         //graphic
 
     }
-    public void foundCamp(int tileId,hex map,Tile tile) {
+    public static void foundCamp(int tileId,hex map,Tile tile) {
         //creat railway
         //graphic
 
     }
-    public void foundPlantation(int tileId,hex map,Tile tile) {
+    public static void foundPlantation(int tileId,hex map,Tile tile) {
         //creat railway
         //graphic
 
     }
-    public void foundQuarry(int tileId,hex map,Tile tile) {
+    public static void foundQuarry(int tileId,hex map,Tile tile) {
         //creat railway
         //graphic
 
     }
-    public void deleteJungle(int tileId,hex map,Tile tile) {
+    public static void deleteJungle(int tileId,hex map,Tile tile) {
         if(map.randFeature[tile]==5){
             map.randFeature[tile]=7;
         }
 
     }
-    public void deleteForest(int tileId,hex map,Tile tile) {
+    public static boolean deleteForest(int tileId,hex map,Tile tile) {
         if (map.randFeature[tileId] == 4) {
             map.randFeature[tileId] = 7;
-
+            return true;
         }
+        else
+            return false;
     }
-    public void deleteMarsh(int tileId,hex map,Tile tile) {
+    public static void deleteMarsh(int tileId,hex map,Tile tile) {
             if(map.randFeature[tileId]==2){
                 map.randFeature[tileId]=7;
         }
 
     }
-    public void deleteRoad(int tileId,hex map,Tile tile) {
+    public static boolean deleteRoad(int tileId,hex map,Tile tile) {
         if (map.randFeature[tileId] == 6) {
             map.randFeature[tileId] = 7;
+            return true;
 
         }
+        else
+            return false;
     }
-    public void repair(int tileId,hex map,Tile tile) {
+    public static void repair(int tileId,hex map,Tile tile) {
 
         }
 
