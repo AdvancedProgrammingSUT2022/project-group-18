5f310a0035bfc74772003e9cb72f5cfd6a5b2906
fatimasd003 <94687837+fatimasd003@users.noreply.github.com>
2022-05-15 13:14:50 +0430

    Add files via upload

--- civilization/src/main/java/model/unit/Worker.java
+++ civilization/src/main/java/model/unit/Worker.java
@@ -1,5 +1,6 @@
 package model.unit;
-
+import model.Tile;
+import model.hex;
 public class Worker extends  Unit{
     public Worker(String name, int movement, int productionCost) {
         this.name = name;
@@ -9,4 +10,89 @@ public class Worker extends  Unit{
         this.productionCost = productionCost;
     }
 
+
+    public static void foundRoad(int tile,hex map) {
+        if((hex.randLand[tile]!=3 )&&(hex.randLand[tile]!= 5 ) &&(hex.randLand[tile]!=6)){
+            //creat road
+            //graphic
+
+        }
+
+
+    }
+    public void foundRailway(int tileId,hex map,Tile tile) {
+        if ((map.randLand[tileId] != 3) && (map.randLand[tileId] != 5) && (map.randLand[tileId] != 6)) {
+            //creat railway
+            //graphic
+        }
+    }
+        public void foundFarm(int tileId,hex map,Tile tile){
+            //creat railway
+            //graphic
+        }
+
+
+    public void foundMine(int tileId,hex map,Tile tile) {
+      //creat railway
+        //graphic
+    }
+    public void foundTradingPost(int tileId,hex map,Tile tile) {
+        //creat railway
+        //graphic
+        tile.incraerseGold(1);
+
+    }
+    public void foundWoodCuting(int tileId,hex map,Tile tile) {
+        //creat railway
+        //graphic
+    }
+    public void foundPasture(int tileId,hex map,Tile tile) {
+        //creat railway
+        //graphic
+
+    }
+    public void foundCamp(int tileId,hex map,Tile tile) {
+        //creat railway
+        //graphic
+
+    }
+    public void foundPlantation(int tileId,hex map,Tile tile) {
+        //creat railway
+        //graphic
+
+    }
+    public void foundQuarry(int tileId,hex map,Tile tile) {
+        //creat railway
+        //graphic
+
+    }
+    public void deleteJungle(int tileId,hex map,Tile tile) {
+        if(map.randFeature[tile]==5){
+            map.randFeature[tile]=7;
+        }
+
+    }
+    public void deleteForest(int tileId,hex map,Tile tile) {
+        if (map.randFeature[tileId] == 4) {
+            map.randFeature[tileId] = 7;
+
+        }
+    }
+    public void deleteMarsh(int tileId,hex map,Tile tile) {
+            if(map.randFeature[tileId]==2){
+                map.randFeature[tileId]=7;
+        }
+
+    }
+    public void deleteRoad(int tileId,hex map,Tile tile) {
+        if (map.randFeature[tileId] == 6) {
+            map.randFeature[tileId] = 7;
+
+        }
+    }
+    public void repair(int tileId,hex map,Tile tile) {
+
+        }
+
+    }
 }
