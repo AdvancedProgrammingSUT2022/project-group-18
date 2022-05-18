d9c9f1152fabeedb2d99975a27827305e9d93df5
mousavi2003 <94689497+mousavi2003@users.noreply.github.com>
2022-05-19 00:55:09 +0430

    Delete src directory

--- src/main/java/model/unit/Producable.java
+++
@@ -1,8 +0,0 @@
-package model.unit;
-
-public interface Producable {
-    public String getName();
-    public int getProductionCost();
-    public int getCurrentProduction();
-    public void addProductionToBuild(int productionToAdd);
-}
