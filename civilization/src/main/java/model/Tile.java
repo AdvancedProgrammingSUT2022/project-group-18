05dee0a7c5d06f29bb4c61271b677a0ee25b0926
fsadatl <mfssmaile@gmail.com>
2022-06-06 16:44:23 +0430

    saveGame

--- civilization/src/main/java/model/Tile.java
+++ civilization/src/main/java/model/Tile.java
@@ -1,22 +1,32 @@
 package model;
 
+import com.google.gson.annotations.SerializedName;
 import model.Resource.Resources;
 import model.improvements.Improvement;
 import model.unit.Unit;
 
 import java.util.ArrayList;
 
-
 public class Tile {
+	@SerializedName("tileType")
 	private String tileType;
+	@SerializedName("isUnderWork")
 	public boolean isUnderWork = false;
+	@SerializedName("coordinate")
 	private float x, y, width, height;
+	@SerializedName("city")
 	private City city;
+	@SerializedName("units")
 	private ArrayList<Unit> units;
+	@SerializedName("cost")
 	private int cost;
+	@SerializedName("resources")
 	private Resources resources = null;
+	@SerializedName("goldOutput")
 	public int goldOutput;
+	@SerializedName("foodOutput")
 	public int foodOutput;
+	@SerializedName("improvments")
 	private static ArrayList<Improvement> improvements = new ArrayList<>();
 
 	public static void setImprovements(Improvement improvements) {
